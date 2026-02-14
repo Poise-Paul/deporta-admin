2680991825bc74ecef85df61ec54863aa81a6a7a
Poise-Paul <stargrades0@gmail.com>
2026-02-14 18:46:30 +0100

    recent-build-updates

---
+++ .next/dev/static/chunks/app_(dashboard)_app-menu_bus-stops_page_tsx_a38a1974._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_b6ead498._.js",
+  "static/chunks/node_modules_a667f8f1._.js"
+],
+    source: "dynamic"
+});
